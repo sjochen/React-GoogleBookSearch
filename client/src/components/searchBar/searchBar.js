a3d38a50efe0b06bfbd4179c5cc2b3fc9e1fca29
Stephen Jochen <stephenjochen2@gmail.com>
2020-02-18 17:43:28 -0500

    made it so the api calls when the search button is hit and it searches for the users input

--- client/src/components/searchBar/searchBar.js
+++ client/src/components/searchBar/searchBar.js
@@ -1,23 +1,30 @@
-import React, { useEffect } from "react"
+import React, { useState } from "react"
 import axios from "axios"
 
 function SearchBar() {
+//State is set using hooks and constants are declared for the search
+    const [input, setInput]= useState('');
 
-    useEffect(() => {
+function HandleClick() {
+    
         axios(
             {
                 method: "GET",
-                url: 'https://www.googleapis.com/books/v1/volumes?q=flowers'
+                url: 'https://www.googleapis.com/books/v1/volumes?q=' + input
                 
             }
-        ).then(res => console.log(res.data))
+        ).then(res => {
+            
+            console.log(res.data)
+        })
 
-    })
+    
+}
     return (
         <div>
             <h2>Book Search</h2>
-            <input type="text" placeholder="Search for a book" name="search" />
-            <button type="submit">Search</button>
+            <input onChange={event => setInput(event.target.value)} type="text" placeholder="Search for a book" name="search" />
+            <button type="submit" onClick={HandleClick}>Search</button>
         </div>
     )
 }
