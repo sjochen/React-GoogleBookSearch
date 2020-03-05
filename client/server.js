00d6ec5df0acf9d71e2d21274e8028bdf3e69a42
Stephen Jochen <stephenjochen2@gmail.com>
2020-03-05 16:57:12 -0500

    working on getting the server to start

---
+++ client/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+const port = 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(port, () => console.log('Server started on port'))
