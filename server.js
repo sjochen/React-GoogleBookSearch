160982ab0fcccdc180aff5471f03e242dd3187be
Stephen Jochen <stephenjochen2@gmail.com>
2020-03-17 13:56:44 -0400

    server runs properly with concurrently. Next step add DB.

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const path = require("path");
 const app = express();
 const Port = 8080;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
-  
-  app.listen(Port, function() {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
+
+app.listen(Port, function () {
     console.log("App listening on PORT " + Port);
-  });
+});
