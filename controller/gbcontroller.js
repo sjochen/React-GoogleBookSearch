61dc54dd13051d4fb327c9954f44809cae85f45f
Stephen Jochen <stephenjochen2@gmail.com>
2020-03-24 13:27:43 -0400

    Set up the controller

---
+++ controller/gbcontroller.js
@@ -0,0 +1,29 @@
+const db = require("../models");
+
+module.exports = {
+    findAll: (req,res) => {
+        db.books.find({})
+            .then(book => res.json(book))
+    },
+
+
+saveBook: (req, res) => {
+    db.books.create({
+        id: req.body.id,
+
+        title: req.body.volumeInfo.title,
+
+        authors: req.body.volumeInfo.authors,
+
+        description: req.body.volumeInfo.description,
+
+        link: req.body.volumeInfo.link
+    }).then(res.json("Complete"))
+},
+
+delete: (req, res) => {
+    db.books.deleteOne({id: req.params.id})
+        .then(res.json("Deleted"))
+}
+
+}
